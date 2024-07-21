c2d97bebe5b83b3610a1d909b382ce526123cb48
Na7aly <tomac.natalia95@gmail.com>
2024-07-21 22:59:26 +0300

    work

--- src/components/SectionLinks/Sectionlinks.jsx
+++ src/components/SectionLinks/Sectionlinks.jsx
@@ -9,36 +9,36 @@ const Sectionlinks = () => {
           <div className={styles.column}>
             <h3>LINK-URI UTILE</h3>
             <ul className={styles.list}>
-              <li><a href="#">Acasa</a></li>
-              <li><a href="#">Despre noi</a></li>
-              <li><a href="#">Domenii</a></li>
-              <li><a href="#">Echipa</a></li>
-              <li><a href="#">Citate STUDEM</a></li>
-              <li><a href="#">Transparentă</a></li>
-              <li><a href="#">Finanțatori</a></li>
-              <li><a href="#">Parteneri</a></li>
+              <li><a href="/">Acasă</a></li>
+              <li><a href="/despre-noi">Despre noi</a></li>
+              <li><a href="/domenii">Domenii</a></li>
+              <li><a href="/echipa">Echipa</a></li>
+              <li><a href="/citate">Citate STUDEM</a></li>
+              <li><a href="/transparenta">Transparență</a></li>
+              <li><a href="/finantatori">Finanțatori</a></li>
+              <li><a href="/parteneri">Parteneri</a></li>
             </ul>
           </div>
           <div className={styles.column}>
             <h3>CONTACTE</h3>
             <ul className={styles.list}>
-              <li>Strada Alexandr Puskin 19, etajul 3</li>
+              <li>Strada Alexandr Pușkin 19, etajul 3</li>
               <li>Moldova</li>
               <li>Bălți</li>
               <li>Telefon: +373 (60) 229996 - Constantin Curca</li>
               <li>Telefon: +373 (79) 971786 - Ioan-Giuliano Ciomschi</li>
-              <li>Email: youth.center@studem.md</li>
-              <li>Email: studem.balti@gmail.com</li>
+              <li>Email: <a href="mailto:youth.center@studem.md">youth.center@studem.md</a></li>
+              <li>Email: <a href="mailto:studem.balti@gmail.com">studem.balti@gmail.com</a></li>
             </ul>
           </div>
           <div className={styles.column}>
             <h3>Despre noi</h3>
             <p>Centrul de Tineret STUDEM este o structură de auto guvernanță a tinerilor din municipiul Bălți înființată în cadrul unui parteneriat Moldo-Danez de lungă durată. Scopul de bază al Centrului este consolidarea democrației în Republica Moldova prin deprinderea cu principiile democratice în rândul tinerilor</p>
             <ul className={styles.socialIcons}>
-              <li><a href="#"><i className="fab fa-twitter"></i></a></li>
-              <li><a href="#"><i className="fab fa-facebook-f"></i></a></li>
-              <li><a href="#"><i className="fab fa-instagram"></i></a></li>
-              <li><a href="#"><i className="fab fa-linkedin-in"></i></a></li>
+              <li><a href="https://twitter.com"><i className="fab fa-twitter"></i></a></li>
+              <li><a href="https://facebook.com"><i className="fab fa-facebook-f"></i></a></li>
+              <li><a href="https://instagram.com"><i className="fab fa-instagram"></i></a></li>
+              <li><a href="https://linkedin.com"><i className="fab fa-linkedin-in"></i></a></li>
             </ul>
           </div>
         </div>
